9428b10cd01e1c8cfefca3b3436bbc9895460003
Roland Ukor <rolandukor@gmail.com>
2015-07-05 01:26:01 +0100

    0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-ssr-app/lein-template "0.1.0"
+(defproject cljs-ssr-app/lein-template "0.1.1-SNAPSHOT"
   :description "Created with lein-create-template"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
